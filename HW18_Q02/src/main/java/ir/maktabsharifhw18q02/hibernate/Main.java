dbffd6a6f4c7fe3146cdbb457bf5a9c9303289f5
Maryam <maryamhabibi868@gmail.com>
2025-04-22 00:33:09 +0330

    Add invalid data to main for test

--- HW18_Q02/src/main/java/ir/maktabsharifhw18q02/hibernate/Main.java
+++ HW18_Q02/src/main/java/ir/maktabsharifhw18q02/hibernate/Main.java
@@ -24,8 +24,8 @@ public class Main {
         TeacherService teacherService = new TeacherService(
                 ApplicationContext.getInstance().getTeacherRepository());
 
-        // Add Student
-        StudentDTO student = new StudentDTO();
+        // Add Valid Student
+        /*StudentDTO student = new StudentDTO();
         student.setFirstName("Ali");
         student.setLastName("Sharif");
         student.setBirthday(LocalDate.of(2000, 1, 1));
@@ -33,10 +33,10 @@ public class Main {
         student.setMajor("IT");
         student.setYearOfEntry(2020);
 
-        studentService.signUp(student);
+        studentService.signUp(student);*/
 
-        // Add Teacher
-        TeacherDTO teacher = new TeacherDTO();
+        // Add Valid Teacher
+       /* TeacherDTO teacher = new TeacherDTO();
         teacher.setFirstName("Maryam");
         teacher.setLastName("Habibi");
         teacher.setBirthday(LocalDate.of(1990, 5, 1));
@@ -45,7 +45,30 @@ public class Main {
         teacher.setLevelOfEducation(PHD);
         teacher.setSalary(35.00);
 
-        teacherService.signUp(teacher);
+        teacherService.signUp(teacher);*/
+
+        // Add Invalid Student
+       /* StudentDTO student1 = new StudentDTO();
+        student1.setFirstName("Ali");
+        student1.setLastName("Sharif");
+        student1.setBirthday(LocalDate.of(2000, 1, 1));
+        student1.setStudentCode("12345678");
+        student1.setMajor("IT");
+        student1.setYearOfEntry(2020);
+
+        studentService.signUp(student1);*/
+
+        // Add Invalid Teacher
+       /* TeacherDTO teacher1 = new TeacherDTO();
+        teacher1.setFirstName("Maryam");
+        teacher1.setLastName("Habibi");
+        teacher1.setBirthday(LocalDate.of(2026, 5, 1));
+        teacher1.setTeacherCode("9876543219");
+        teacher1.setDegree("Professor Science");
+        teacher1.setLevelOfEducation(PHD);
+        teacher1.setSalary(35.00);
+
+        teacherService.signUp(teacher1);*/
 
 
     }
